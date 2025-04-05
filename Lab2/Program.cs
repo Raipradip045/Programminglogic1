f5f3872866810d582c4955584f923b39867f3375
Raipradip045 <raipradip045@gmail.com>
2025-04-05 21:52:14 +0000

    lab 2

---
+++ Lab2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int myInt=9;
+        double myDouble=20.1;
+        float myFloat=22.5f;
+        char myChar="A";
+        bool myBool=true;
+        string myString="Hello";
+        Console.WriteLine(myINt);
+        Console.WriteLine(myDouble);
+        Console.WriteLine(MyFloat);
+        Console.WriteLine(myChar);
+        Console.WriteLine(myBool);
+        Console.WriteLine(myString);
+
+        double doubleValue=9.78;
+        int intValue=(int)doubleValue;
+        string intToString=Convert.intToString(intvalue);
+        Console.WriteLine("double:"+doubleValue);
+        Console.WriteLine("int:"+intValue);
+         Console.WriteLine("int to string:"+intToString);
+          Console.WriteLine("bool to string:"+boolToString);
+        Console.Write("Enter your name:");
+        string name=Console.ReadLine();
+       
+        Console.Write("Enter your age:");
+        int age=Convert.ToInt32(Console.ReadLine);
+        Console.WriteLine("Hello(name)you are (age)years old.");
+       
+        num1=6
+        numb2=10
+        Calculate addition_result=num1+num2+10.
+        Calculate subtraction_result=num1-num2-2.
+        Calculate Multiplication_result=(num*num2)*3.
+        Calculate division_result=(num1/num2)/2.
+        Calculate 
+
+    
+
+
+           
+    }
+}
