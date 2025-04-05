6a92bac29437db0182212a12433149ee5cd5994e
Raipradip045 <raipradip045@gmail.com>
2025-04-05 02:30:27 +0000

    lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Lab1;
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography;
+
+namespace Lab1;
 
 class Program
 {
@@ -6,6 +11,30 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("welcome to IT-1050!");
+        // Step 1
+        Console.WriteLine("PradipRai");
+        Console.WriteLine("IT1050")
+      // Step 2  
+        Name: PradipRai
+        Title:IT-1050-Lab1
+        // Step 3
+        int favoriteNumber = 10;
+        string favoriteLanguage = "C#";
+        double numberOfPrograms = 11;
+        bool hasPorgrammingExperience = true;
+        Console.WriteLine("Favorite Number: "+ 10);
+        Console.WriteLine("Favorite Programming Language:"C#"");
+        Console.WriteLine("Number of Programs: "+ 11");
+        Console.WriteLine("Has Programming Experience:"+ True");
+        //Step 5
+        const string schoolName="Tri-c";
+        Console.WriteLine("School Name:"+Tri-c");
+
+
+    
+
+
+        
     }
     
 }
