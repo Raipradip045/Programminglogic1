1776723a2387cf9cb477a1b485c9004a0da55615
Raipradip045 <raipradip045@gmail.com>
2025-04-14 01:27:04 +0000

    Lab 3

---
+++ Lab3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+         int a=10, b=5, c=15;
+        Console.WriteLine("a>b:"+ isGreaterThanB);
+        Console.writeLine("a<c:"+ islessThanC);
+        Console.WriteLine("a>b & a>c:"+ isgreaterThanB&C);
+
+
+        isRaining=True
+        haveUmbrella=False
+        if isRaining and not have umbrella: print("Take an umbrella!")
+        Else: print("you'are good to go!")
+
+
+        Console.Write("Enter your age:");
+        int age= Convert.ToInt32(Console.ReadLine());
+        if(age<5)
+        Console.writeLine("Ticket is free!");
+        else if (age>=5&& age<=12)
+        Console.writeLine("Child ticket:$5");
+        else if(age>=13 && age <=64)
+        Console.writeLine("standard  ticket: $10");
+        else if(age >= 65)
+        Console.WriteLine("Senior  ticket: $6");
+
+    }
+}
