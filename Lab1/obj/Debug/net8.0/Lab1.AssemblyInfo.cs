6fc9fbdf8e6a04d63ffa92463e5ef889550426c3
Raipradip045 <raipradip045@gmail.com>
2025-05-11 20:34:50 +0000

    Lab 8

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa0cfc314227d01fc650b0bc840222e7da7ad47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f02e992cf6e794cb0c9e6ad92b1d74f52f4b170")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab8--use-program-main/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Step 1
+Using System;
+class Animal
+{
+    public virtual void AnimalSound()// Virtual method
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+//Step 2
+class Cat: Animal 
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("The cat says:meow");
+    }
+}
+class Dog: Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    })
+}
+
+//Step 3
+class Program
+{
+    static void Main(string[]args)
+    {
+        Animal myAnimal;
+
+        myAnimal = new Animal();
+        myAnimal.AnimalSound();
+
+        myAnimal = new Cat();
+        myAnimal.AnimalSound();
+
+        myAnimal = new Dog();
+        myAnimal.AnimalSound();
+    }
+}
+
+
+
